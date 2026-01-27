25a47b1f58ce41841da181ce43d0549b8739bd5f
IaraTassi <iaratassi@hotmail.com>
2026-01-27 12:31:16 -0300

    chore: configure environment and database schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log(`🚀 O servidor está executando na porta 3000`);
+});
